5a05eaeb3d6875e1250b010eda5c38eb99e34f75
Cera White <cwhite@anigramsproductions.com>
2026-03-03 02:21:02 -0800

    Update generate script to upload to web server via FTP

--- generate.rb
+++ generate.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 require 'yaml'
 require 'json'
+require 'net/ftp'
+require 'openssl'
 require_relative './lib/parsers/yen_press_parser'
 require_relative './lib/parsers/seven_seas_parser'
 require_relative './lib/parsers/square_enix_parser'
@@ -43,6 +45,39 @@ puts "Done getting series data."
 
 puts "Building release calendar..."
 calendar_file_name = "book-releases.ics"
-builder = IcsFeedBuilder.new(domain: "anigramsproductions.com", calname: "Book Releases")
+domain_name = "anigramsproductions.com"
+builder = IcsFeedBuilder.new(domain: domain_name, calname: "Book Releases")
 builder.write(calendar_file_name, series_list)
 puts "Calendar generated at: #{calendar_file_name}"
+
+ftp_host = ENV.fetch("FTP_HOST")
+ftp_user = ENV.fetch("FTP_USERNAME")
+ftp_pass = ENV.fetch("FTP_PASSWORD")
+
+puts "Connecting to FTP host: #{ftp_host}..."
+
+ctx = OpenSSL::SSL::SSLContext.new
+ctx.verify_mode = OpenSSL::SSL::VERIFY_PEER
+ctx.ca_file = "/etc/ssl/certs/ca-certificates.crt"
+
+ftp = Net::FTP.new(
+  ftp_host,
+  port: 21,
+  ssl: true,
+  private_data_connection: true,
+  ssl_context: ctx
+)
+
+ftp.debug_mode = true
+
+ftp.passive = true
+ftp.login(ftp_user, ftp_pass)
+
+puts "Uploading release calendar..."
+remote_path = "personal/calendars/#{calendar_file_name}"
+ftp.putbinaryfile(calendar_file_name, remote_path)
+
+ftp.close
+
+final_path = "https://#{domain_name}/personal/calendars/#{calendar_file_name}"
+puts "Done! Release calendar is now available at: #{final_path}"
